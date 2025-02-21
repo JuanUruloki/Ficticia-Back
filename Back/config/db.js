87be6f9dda06802208aded71e196b8fa349f3b27
JuanUruloki <juampy_1@live.com>
2025-02-21 17:47:42 -0300

    fix_changes for db deploy

--- Back/config/db.js
+++ Back/config/db.js
@@ -1,18 +1,28 @@
 const { Sequelize } = require("sequelize");
 const config = require("./config.json")[process.env.NODE_ENV || "development"];
 
-const sequelize = new Sequelize(config.database, config.username, config.password, {
-  host: config.host,
-  dialect: config.dialect,
-});
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
+  : new Sequelize(config.database, config.username, config.password, {
+      host: config.host,
+      dialect: config.dialect,
+    });
 
 const connectDB = async () => {
   try {
     await sequelize.authenticate();
-    console.log("Conexión a la base de datos establecida con éxito.");
+    console.log("✅ Conexión a la base de datos establecida con éxito.");
   } catch (error) {
-    console.error("No se pudo conectar a la base de datos:", error);
-    process.exit(1); // Salir si la conexión falla
+    console.error("❌ No se pudo conectar a la base de datos:", error);
+    process.exit(1);
   }
 };
 
